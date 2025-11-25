bc03e8190bc99ad2d3ce8f994f394caa784c4b87
Your Name <you@example.com>
2025-11-25 14:18:47 +0100

    add while loop

--- get_bikes.R
+++ get_bikes.R
@@ -1,2 +1,13 @@
 ### 
-print("hej world")
+
+runcondition=TRUE
+counter=0
+
+while (runcondition == TRUE){
+  print(Sys.time())
+  Sys.sleep(5)
+  counter=counter+1
+  if (counter > 10){
+    runcondition = FALSE
+  }
+}
